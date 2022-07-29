e9c5bbc6644ea8bb9e9d15f1b7767c05e9d09ca8
QunFan <allenfan1012@gmail.com>
2022-07-29 22:25:38 +1000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <= 400)
+  return 'This one is on me!';
+  // Write your code here!
+  else if (ride > 2000 && ride < 2500)
+  return 'I will gladly take your thirty bucks.'
+  else if(ride >2500)
+  return 'No can do.'
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return 'Bye.';
+      
+  }
   // Write your code here!
 }
